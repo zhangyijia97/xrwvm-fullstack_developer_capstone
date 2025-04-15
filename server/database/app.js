2b4ed82607e24fce8acde9ae494c1ffa04c393c9
zhangyijia97 <zhangyijia1997@gmail.com>
2025-04-15 07:40:40 -0400

    JS linter 2

--- server/database/app.js
+++ server/database/app.js
@@ -3,11 +3,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const fs = require('fs');
-const  cors = require('cors')
-const app = express()
+const  cors = require('cors');
+const app = express();
 const port = 3030;
 
-app.use(cors())
+app.use(cors());
 app.use(require('body-parser').urlencoded({ extended: false }));
 
 const reviews_data = JSON.parse(fs.readFileSync("reviews.json", 'utf8'));
@@ -35,7 +35,7 @@ try {
 
 // Express route to home
 app.get('/', async (req, res) => {
-    res.send("Welcome to the Mongoose API")
+    res.send("Welcome to the Mongoose API");
 });
 
 // Express route to fetch all reviews
@@ -94,8 +94,8 @@ app.get('/fetchDealer/:id', async (req, res) => {
 //Express route to insert review
 app.post('/insert_review', express.raw({ type: '*/*' }), async (req, res) => {
   data = JSON.parse(req.body);
-  const documents = await Reviews.find().sort( { id: -1 } )
-  let new_id = documents[0]['id'] in+1
+  const documents = await Reviews.find().sort( { id: -1 } );
+  let new_id = documents[0]['id'] in+1;
 
   const review = new Reviews({
 		"id": new_id,
